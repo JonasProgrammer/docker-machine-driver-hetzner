c99eeca9edba53d0275a045f760f7fe009bfc53d
JonasS <jonass@dev.jsje.de>
2017-11-03 00:34:34 +0100

    Changed repository name to match driver format

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	"github.com/jonasprogrammer/docker-machine-hetzner-plugin/driver"
+	"github.com/jonasprogrammer/docker-machine-driver-hetzner/driver"
 )
 
 func main() {
