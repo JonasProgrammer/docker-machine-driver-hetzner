1e16650369913e432bdc215f96105338bbf478a5
Max Schmitt <max@schmitt.mx>
2018-10-25 21:42:38 +0200

    added version flag (fix: #25) (#26)

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/docker/machine/libmachine/drivers/plugin"
 )
 
+// Version will be added once we start the build process via travis-ci
+var Version string
+
 func main() {
+	version := flag.Bool("v", false, "prints current docker-machine-driver-hetzner version")
+	flag.Parse()
+	if *version {
+		fmt.Printf("Version: %s\n", Version)
+		os.Exit(0)
+	}
 	plugin.RegisterDriver(NewDriver())
 }
